9a10fcba3f33b18abb3680976437611a56bcda55
jcksnmdy <38597347+jcksnmdy@users.noreply.github.com>
2019-07-07 00:55:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,100 +1,12 @@
-var randomnum, firstQ, secondQ;
+var randomnum, firstQ, secondQ, test;
+test = 0
 function um() {
     alert("Hey")
+    test = 1
 }
-function sendMail() {
-    randomnum = Math.floor(Math.random(0)*10);
-    firstQ = randomnum
-    if (randomnum == 0) {
-        document.getElementById('q').innerHTML = "What is the apital of America?";
+mainClock = setInterval(mainFunction, 20)
+function mainFunction() {
+    if (test == 1) {
+        alert("More thank one")
     }
-    if (randomnum == 1) {
-        document.getElementById('q').innerHTML = "What is the capital of England?";
-    }
-    if (randomnum == 2) {
-        document.getElementById('q').innerHTML = "What is the capital of Argentina";
-    }
-    if (randomnum == 3) {
-        document.getElementById('q').innerHTML = "What is the capital of Ireland";
-    }
-    if (randomnum == 4) {
-        document.getElementById('q').innerHTML = "What is the capital of Scotland";
-    }
-    if (randomnum == 5) {
-        document.getElementById('q').innerHTML = "What is the capital of Iceland";
-    }
-    if (randomnum == 6) {
-        document.getElementById('q').innerHTML = "What is the capital of Japan";
-    }
-    if (randomnum == 7) {
-        document.getElementById('q').innerHTML = "What is the capital of France";
-    }
-    if (randomnum == 8) {
-        document.getElementById('q').innerHTML = "What is the capital of Australia";
-    }
-    if (randomnum == 9) {
-        document.getElementById('q').innerHTML = "What is the capital of China";
-    }
-    console.log(randomnum)
-    var link = "mailto:" + escape(document.getElementById('player1').value)
-             + "?subject=Question"
-             + "&body=" + escape(document.getElementById('q').value)
-    ;
-    window.location.href = link;
-}
-function sendMail2() {
-    var link = "mailto:" + escape(document.getElementById('player2').value)
-             + "?subject=Question"
-             + "&body=" + escape(document.getElementById('q').value)
-    ;
-    window.location.href = link;
-    console.log(randomnum)
-}
-function sendMail3() {
-    randomnum = Math.floor(Math.random(0)*10);
-    secondQ = randomnum
-    if (randomnum == 0 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the apital of America?";
-    }
-    if (randomnum == 1 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of England?";
-    }
-    if (randomnum == 2 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Argentina";
-    }
-    if (randomnum == 3 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Ireland";
-    }
-    if (randomnum == 4 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Scotland";
-    }
-    if (randomnum == 5 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Iceland";
-    }
-    if (randomnum == 6 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Japan";
-    }
-    if (randomnum == 7 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of France";
-    }
-    if (randomnum == 8 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of Australia";
-    }
-    if (randomnum == 9 && randomnum == firstQ) {
-        document.getElementById('q').innerHTML = "What is the capital of China";
-    }
-    console.log(randomnum)
-    var link = "mailto:" + escape(document.getElementById('player3').value)
-             + "?subject=Question"
-             + "&body=" + escape(document.getElementById('q').value)
-    ;
-    window.location.href = link;
-}
-function sendMail4() {
-    var link = "mailto:" + escape(document.getElementById('player4').value)
-             + "?subject=Question"
-             + "&body=" + escape(document.getElementById('q').value)
-    ;
-    window.location.href = link;
-    console.log(randomnum)
 }
